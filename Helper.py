14a504580e1d925986d629ca8a07debe9634fe9a
clecle226 <clementcachinho226@msn.com>
2019-01-23 07:34:11 +0100

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -1,2 +1,15 @@
-class MainWindow(QMainWindow):
+from subprocess import Popen, PIPE
+
+class DeviceHelper():
+    Processus = None
+    Log = None
+    IMEI1 = None
+    IMEI2 = None
+    SerialNumber = None
+
     def __init__(self):
+        Processus = subprocess.Popen("adb shell")
+    def ShellIn(self, Message):
+        Processus.communicate(Message, time)
+
+    #def ShellOut(self):
